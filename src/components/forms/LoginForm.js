8f999675b1ed83b7cc36307746d75bdf03d2b20d
vidhdhi <vidhdhiyatharans@gmail.com>
2020-06-23 08:56:42 +0530

    login validation

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -5,9 +5,9 @@ import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import Button from "@material-ui/core/Button";
 import TextField from '@material-ui/core/TextField';
-import { Formik } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import { useFormik } from 'formik';
-
+import * as Yup from 'yup';
 const useStyles = makeStyles((theme) =>
   createStyles({
     forgotText: {
@@ -28,50 +28,61 @@ const useStyles = makeStyles((theme) =>
       maxWidth: '430px',
       width: '100%',
     },
+    Field:{
+      width: '100%',
+      height:'100%',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
   })
 );
+
+const SignupSchema  = Yup.object().shape({
+  firstName: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+
+  email: Yup.string()
+    .email('Invalid email')
+    .required('Required'),
+});
+
 
 const LoginForm = (props) => {
   const classes = useStyles();
 
-  const formik = useFormik({
-    initialValues: {
-      firstName: '',
-    
-      email: '',
-    },
-    onSubmit: values => {
-      alert(JSON.stringify(values, null, 2));
-    },
-  });
-  return (
-    <form onSubmit={formik.handleSubmit} className={classes.formCart}>
-      
-      <label htmlFor="firstName">First Name</label>
-      <TextField
-        id="firstName"
-        name="firstName"
-        type="text"
-        onChange={formik.handleChange}
-        value={formik.values.firstName}
-        fullWidth
-        variant="outlined"
-        placeholder='Enter Name'
-      />
   
-      <label htmlFor="email">Email Address</label>
-      <TextField
-        id="email"
-        name="email"
-        type="email"
-        onChange={formik.handleChange}
-        value={formik.values.email}
-        fullWidth
-        variant="outlined"
-        placeholder='Enter password'
-      />
-      <Button fullWidth variant="contained" color="primary" type="submit">Submit</Button>
-    </form>
+  return (
+    
+    <Formik
+    initialValues={{
+      firstName: '',
+      
+      email: '',
+    }}
+    validationSchema={SignupSchema}
+    onSubmit={values => {
+      // same shape as initial values
+      console.log(values);
+    }}
+  >
+    {({ errors, touched }) => (
+      <Form>
+        <Field name="firstName"   fullWidth
+        variant="outlined"/>
+        {errors.firstName && touched.firstName ? (
+          <div>{errors.firstName}</div>
+        ) : null}
+      
+        <Field name="email" type="email" fullWidth
+        variant="outlined"/>
+        {errors.email && touched.email ? <div>{errors.email}</div> : null}
+        <Button fullWidth variant="contained" color="primary" type="submit">Submit</Button>
+      </Form>
+    )}
+  </Formik>
   );
 };
 export default LoginForm;
