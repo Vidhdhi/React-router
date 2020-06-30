970bd962f7bc11e0b74c9ee81d7fcb5b9af98837
vidhdhi <vidhdhiyatharans@gmail.com>
2020-06-30 11:53:48 +0530

    test

--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
+import InputField from '../components/core/Input';
+import ButtonField from '../components/core/Button';
+
+import Grid from '@material-ui/core/Grid';
+
+
 class Test extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fruits: ['mango']
+      users: ['zajith@huex.studio', 'gobi@huex.studio', 'chanthan@huex.studio']
     }
-    this.selectFruit = this.selectFruit.bind(this);
+   
   }
-
-  selectFruit(e) {
-    if(e.target.checked) {
-      this.setState({
-        fruits: [ ...this.state.fruits, e.target.value],
-      }, () => {
-        console.log(this.state.fruits);
-      });
-    }
+  
+  handleChange = (event) => {
+    this.setState({users: event.target.value})
+    
   }
-
   render() {
+    const { users } = this.state;
     return (
       <div>
         <form>
-          <input type="checkbox" id="apple" name="fruit" value="Apple"
-          		onClick={this.selectFruit}/>Apple <br/>
-          <input type="checkbox" id="mango" name="fruit" value="Mango"
-          		onClick={this.selectFruit}/>Mango <br/>
-          <input type="checkbox" id="pear" name="fruit" value="Pear"
-          		onClick={this.selectFruit}/>Pear <br/>
-        </form>
+        <Grid container item xs={12} sm={12} md={12} lg={12}>
+        <InputField
+          id={'email'}
+          name={'email'}
+          type={'email'}
+          label={'Email address'}
+          placeholder={'Enter email address'}
+          onChange={this.handleChange}
+           value={users}
+         />
+      </Grid>
+      </form>
+      <ButtonField>
+        add
+      </ButtonField>
       </div>
     );
   }
