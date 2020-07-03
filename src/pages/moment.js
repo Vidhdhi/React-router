126b47186c0cbf9becb9169b728c0574b74fd1b1
vidhdhi <vidhdhiyatharans@gmail.com>
2020-07-03 08:54:09 +0530

    Update moment.js

--- src/pages/moment.js
+++ src/pages/moment.js
@@ -13,6 +13,17 @@ const useStyles = makeStyles((theme) => ({
     width: 200,
   },
 }));
+let handleStartInput = (value)  =>{
+    this.setState({
+      startDate: value
+    });
+  },
+
+  let handleEndInput = (value) => {
+    this.setState({
+      endDate: value
+    });
+  },
 
 export default function DateAndTimePickers() {
   const classes = useStyles();
@@ -25,10 +36,25 @@ export default function DateAndTimePickers() {
         type="datetime-local"
         defaultValue="2017-05-24T10:30"
         className={classes.textField}
+        value={this.state.startDate}  
         InputLabelProps={{
           shrink: true,
         }}
       />
+
+<TextField
+        id="datetime-local"
+        label="Next appointment"
+        type="datetime-local"
+        defaultValue="2017-05-24T10:30"
+        className={classes.textField}
+        value={this.state.endDate}
+        InputLabelProps={{
+          shrink: true,
+        }}
+      />
+
+
     </form>
   );
 }
